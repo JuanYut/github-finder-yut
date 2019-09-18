bcf1c7ad3c260411e354761774647165f6f0d516
JuanYut <juanyut97@hotmail.com>
2019-09-18 14:29:55 -0500

    peticion cambiada con axios y componente search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 
 class App extends Component {
   state = {
@@ -9,28 +11,35 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
-    console.log(this.state.users);
+  // searchUsers = async text => {
+  //   let data;
+  //   await fetch(
+  //     `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   )
+  //     .then(response => response.json())
+  //     .then(jsonThing => (data = jsonThing));
+
+  //   this.setState({ users: data, loading: false });
+  // };
+
+  // * Search github users
+  searchUsers = async text => {
     this.setState({ loading: true });
 
-    let data;
-    await fetch('https://api.github.com/users')
-      .then(response => response.json())
-      .then(jsonThing => (data = jsonThing));
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
-    // await fetch(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-    //   .then(response => response.json())
-    //   .then(jsonThing => (data = jsonThing));
-
-    this.setState({ users: data, loading: false });
+    this.setState({ users: res.data.items, loading: false });
     console.log(this.state.users);
-  }
+  };
 
   render() {
     return (
       <div className='App'>
         <Navbar title='GitHub Finder' />
         <div className='container'>
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
